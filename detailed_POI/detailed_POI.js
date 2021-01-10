999cbce0ce1cdcc756094e6e51c6cf42f341a90b
Mahdi <mahdirefaideen@gmail.com>
2021-01-10 23:19:09 +0530

    POI complete

--- detailed_POI/detailed_POI.js
+++ detailed_POI/detailed_POI.js
@@ -1,6 +1,54 @@
 var slideIndex = 1;
 
+// var POI_Object =
+//     {
+//         productID: 1,
+//         shop: "Nufail bun shop",
+//         picture: ["bun.jpg", "cart.png", "cart2.png", "heart.png", "heart2.png", "Sample.mp4"],
+//         name: "Chicken Submarine",
+//         description: "Home made pasta with sun kissed milk sauce, chilli, olive oil and egg all mixed together Italian pasta....",
+//         price: 200,
+//         rating: 4,
+//         isFavourite : false
+//     }
+
 $(document).ready(function () {
+
+    ListOfPOI = JSON.parse(localStorage.POI_Object);
+    console.log(ListOfPOI[0]);
+
+    var slideShow = '';
+    var slideDot = '<div class="dot-list">';
+    $.each(ListOfPOI[0].picture, function (index, item) {
+        slideShow += '<div class="mySlides fade">';
+        slideShow += '<div class="numbertext">'+(index+1)+' / ' + ListOfPOI[0].picture.length + '</div>';
+        slideShow += '<img src="../sub1.jpg" style="width:100%">';
+        slideShow += '</div>';
+        slideDot += '<span class="dot" onclick="currentSlide('+(index+1)+')"></span>';
+    });
+    slideDot += '</div>';
+    $("#slide-show").html(slideShow);
+    $("#slide-dot").html(slideDot);
+
+    
+
+    var setTitle = '<div class="title">'+ ListOfPOI[0].name +'</div>';
+    var setShop = '<div class="shop">By <b>'+ ListOfPOI[0].shop+'</b></div>';
+    var setDesc = '<div class="description">'+ListOfPOI[0].description+'</div>';
+    var setItemPrice = '<div class="item-price">'+ListOfPOI[0].price+'</div>'
+    $("#set-title").html(setTitle);
+    $("#set-shop").html(setShop);
+    $("#set-desc").html(setDesc);
+    $("#set-item-price").html(setItemPrice);
+    
+
+
+    // $.getJSON("test.json", function(data){
+    //     console.log(data.name); // Prints: Harry
+    //     console.log(data.age); // Prints: 14
+    // }).fail(function(){
+    //     console.log("An error has occurred.");
+    // });
 
     showSlides(slideIndex);
 
